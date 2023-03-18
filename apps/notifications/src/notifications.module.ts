a6fa3046b0039c89e7afa9fb736ca51e03d8937a
Michael <guay.me@gmail.com>
2023-03-18 12:13:30 -0400

    App working on local K8s

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -11,6 +11,10 @@ import { NotificationsService } from './notifications.service';
       isGlobal: true,
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
+        GOOGLE_OAUTH_CLIENT_ID: Joi.string().required(),
+        GOOGLE_OAUTH_CLIENT_SECRET: Joi.string().required(),
+        GOOGLE_OAUTH_REFRESH_TOKEN: Joi.string().required(),
+        SMTP_USER: Joi.string().required(),
       }),
     }),
     LoggerModule,
