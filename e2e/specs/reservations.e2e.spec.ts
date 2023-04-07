e0416f8506131d7145d4a117aa1f6ec569e67169
Michael <guay.me@gmail.com>
2023-04-07 12:06:09 -0400

    Update E2E command

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -23,31 +23,47 @@ describe('Reservations', () => {
     jwt = await response.text();
   });
 
-  test('Create', async () => {
-    const response = await fetch('http://reservations:3000/reservations', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authentication: jwt,
-      },
-      body: JSON.stringify({
-        startDate: '02-01-2023',
-        endDate: '02-05-2023',
-        placeId: '123',
-        invoiceId: '123',
-        charge: {
-          amount: 13,
-          card: {
-            cvc: '413',
-            exp_month: 12,
-            exp_year: 2027,
-            number: '4242 4242 4242 4242',
-          },
+  test('Create & Get', async () => {
+    const createdReservation = await createReservation();
+    const responseGet = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          Authentication: jwt,
         },
-      }),
-    });
-    // expect(response.ok).toBeTruthy();
-    const reservation = await response.json();
-    console.log(reservation);
+      },
+    );
+    const reservation = await responseGet.json();
+    expect(createdReservation).toEqual(reservation);
   });
+
+  const createReservation = async () => {
+    const responseCreate = await fetch(
+      'http://reservations:3000/reservations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+        body: JSON.stringify({
+          startDate: '02-01-2023',
+          endDate: '02-05-2023',
+          placeId: '123',
+          invoiceId: '123',
+          charge: {
+            amount: 13,
+            card: {
+              cvc: '413',
+              exp_month: 12,
+              exp_year: 2027,
+              number: '4242 4242 4242 4242',
+            },
+          },
+        }),
+      },
+    );
+    expect(responseCreate.ok).toBeTruthy();
+    return responseCreate.json();
+  };
 });
